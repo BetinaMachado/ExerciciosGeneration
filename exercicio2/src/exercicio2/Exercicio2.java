7f787c59d49caab1f8708593fbcddcee4587e2a7
BetinaMachado <100873819+BetinaMachado@users.noreply.github.com>
2022-05-06 23:50:22 -0300

    Add files via upload

---
+++ exercicio2/src/exercicio2/Exercicio2.java
@@ -0,0 +1,29 @@
+package exercicio2;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			Scanner ler = new Scanner(System.in); 
+			Double n1, n2, n3;
+			
+			System.out.println("Digite o primeiro número: ");
+			n1=ler.nextDouble(); 
+			System.out.println("Digite o segundo número: "); 
+			n2=ler.nextDouble(); 
+			System.out.println("Digite o terceiro número: "); 
+			n3=ler.nextDouble();
+		
+			//SortedSet<E>
+			
+			//SortedSet(n1, n2, n3); 
+			//System.out.println("Ordem crescente dos números: ", SortedSet); 
+		}
+		//Ou precisa criar uma variável?? Ou colocar os parâmetros do sortedset dentro do print?
+
+
+	}
+
+}
